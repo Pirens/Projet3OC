d0bf60cf1ef454f2472af777b79d118458674648
Pirens <pvfesnault@gmail.com>
2018-12-31 19:32:35 +0100

    Brut mapping and base movements

---
+++ main.py
@@ -0,0 +1,54 @@
+# Help MacGyver to escape
+
+# Brut mapping
+map = [
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
+['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+]
+
+# Starting position
+ord = 0
+abs = 0
+map[ord][abs] = 'P'
+
+# User entry
+print("Entrer D")
+user_entry = input()
+# move right
+if user_entry == 'R':
+    map[ord][abs] = 'X'
+    abs = abs + 1
+    map[ord][abs] = 'P'
+
+# move left
+elif user_entry == 'L':
+    map[ord][abs] = 'X'
+    abs = abs - 1
+    map[ord][abs] = 'P'
+
+# move up
+elif user_entry == 'U':
+    map[ord][abs] = 'X'
+    ord = ord - 1
+    map[ord][abs] = 'P'
+
+# move down
+elif user_entry == 'D':
+    map[ord][abs] = 'X'
+    ord = ord + 1
+    map[ord][abs] = 'P'
+
+print(map)
