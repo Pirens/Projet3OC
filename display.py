278d3345b90d71e6b7b4a56f98e36c244d522292
Pirens <pvfesnault@gmail.com>
2019-02-03 10:43:53 +0100

    Ending the game

--- display.py
+++ display.py
@@ -2,14 +2,59 @@ import json
 import pygame
 from pygame.locals import *
 
+from object import Object
+from macgyver import Macgyver
+
 class Display:
 
     def __init__(self):
         with open('map.json', 'r') as f:
             self.mapf = json.load(f)
+            pygame.init()
+            self.needle = Object(1,1,'N')
+            self.mapf = self.needle.object_dispatch(self.mapf)
+            self.tube = Object(1,1,'T')
+            self.mapf = self.tube.object_dispatch(self.mapf)
+            self.ether = Object(1,1,'E')
+            self.mapf = self.ether.object_dispatch(self.mapf)
+
 
     def display_map(self):
-        for affichage in main.display.mapf:
-            for points in affichage:
-                print(points, end =" ")
-            print("")
+        window = pygame.display.set_mode((600,600), RESIZABLE)
+        line_index = 0
+        ord = 0
+        for line in self.mapf:
+            # Read char in lines
+            char_index = 0
+            abs = 0
+            for char in line:
+                # Display a sprite in function of a wall or a tile
+                if self.mapf[char_index][line_index] == 'O':
+                    sprite_w = pygame.image.load("ressources/wall.png").convert()
+                    window.blit(sprite_w, (ord,abs))
+                else:
+                    sprite_t = pygame.image.load("ressources/tile.png").convert()
+                    window.blit(sprite_t, (ord,abs))
+                    # Ojects and characters picture on tile
+                    if self.mapf[char_index][line_index] == 'G':
+                        picture = pygame.image.load("ressources/gardien.png").convert_alpha()
+                        window.blit(picture, (ord,abs))
+                    elif self.mapf[char_index][line_index] == 'N':
+                        picture = pygame.image.load("ressources/aiguille.png").convert_alpha()
+                        window.blit(picture, (ord,abs))
+                    elif self.mapf[char_index][line_index] == 'T':
+                        picture = pygame.image.load("ressources/tube_plastique.png").convert()
+                        picture.set_colorkey((255,255,255))
+                        window.blit(picture, (ord,abs))
+                    elif self.mapf[char_index][line_index] == 'E':
+                        picture = pygame.image.load("ressources/ether.png").convert()
+                        picture.set_colorkey((1,1,1))
+                        window.blit(picture, (ord,abs))
+                    elif self.mapf[char_index][line_index] == 'P':
+                        character = pygame.image.load("ressources/macgyver.png").convert_alpha()
+                        window.blit(character, (ord,abs))
+                abs = abs + 40
+                char_index = char_index + 1
+            ord = ord + 40
+            line_index = line_index + 1
+        pygame.display.flip()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pygame
+from pygame.locals import *
 ''' Local import '''
 from display import Display
 from macgyver import Macgyver
@@ -12,67 +14,41 @@ class Main:
 
         self.display = Display()
         self.character = Macgyver(1,1)
-        self.needle = Object(1,1,'N')
-        self.display.mapf = self.needle.object_dispatch(self.display.mapf)
-        self.tube = Object(1,1,'T')
-        self.display.mapf = self.tube.object_dispatch(self.display.mapf)
-        self.ether = Object(1,1,'E')
-        self.display.mapf = self.ether.object_dispatch(self.display.mapf)
-
 
     def play(self):
 
-        for affichage in self.display.mapf:
-            for points in affichage:
-                print(points, end =" ")
-            print("")
+        self.display.display_map()
         nbr_objects = 0
         while self.display.mapf[self.character.ord][self.character.abs] != self.display.mapf[13][13]:
-            print("Can you help MacGyver to asleep the guardian ?")
-            print("You can use D for right, Q for left, Z for up and S for down")
-            user_entry = input()
-
-            # move right + checking if an object is a the position before
-            if user_entry == 'D':
-                if (self.display.mapf[self.character.ord][self.character.abs + 1] == 'N')\
-                or (self.display.mapf[self.character.ord][self.character.abs + 1] == 'T')\
-                or (self.display.mapf[self.character.ord][self.character.abs + 1] == 'E'):
-                    nbr_objects = nbr_objects + 1
-                self.display.mapf = self.character.right(self.display.mapf)
-
-            # move left + checking if an object is a the position before
-            elif user_entry == 'Q':
-                if (self.display.mapf[self.character.ord][self.character.abs - 1] == 'N')\
-                or (self.display.mapf[self.character.ord][self.character.abs - 1] == 'T')\
-                or (self.display.mapf[self.character.ord][self.character.abs - 1] == 'E'):
-                    nbr_objects = nbr_objects + 1
-                self.display.mapf = self.character.left(self.display.mapf)
-
-            # move up + checking if an object is a the position before
-            elif user_entry == 'Z':
-                if (self.display.mapf[self.character.ord - 1][self.character.abs] == 'N')\
-                or (self.display.mapf[self.character.ord - 1][self.character.abs] == 'T')\
-                or (self.display.mapf[self.character.ord - 1][self.character.abs] == 'E'):
-                    nbr_objects = nbr_objects + 1
-                self.display.mapf = self.character.up(self.display.mapf)
-
-            # move down + checking if an object is a the position before
-            elif user_entry == 'S':
-                if (self.display.mapf[self.character.ord + 1][self.character.abs] == 'N')\
-                or (self.display.mapf[self.character.ord + 1][self.character.abs] == 'T')\
-                or (self.display.mapf[self.character.ord + 1][self.character.abs] == 'E'):
-                    nbr_objects = nbr_objects + 1
-                self.display.mapf = self.character.down(self.display.mapf)
-
-            # in case of a wrong caracter
-            else:
-                print("Wrong entry!")
-
-            # return map every time
-            for affichage in self.display.mapf:
-                for points in affichage:
-                    print(points, end =" ")
-                print("")
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.display.mapf[self.character.ord][self.character.abs] = self.display.mapf[13][13]
+                if event.type == KEYDOWN:
+                    if event.key == K_RIGHT:
+                        if (self.display.mapf[self.character.ord][self.character.abs + 1] == 'N')\
+                        or (self.display.mapf[self.character.ord][self.character.abs + 1] == 'T')\
+                        or (self.display.mapf[self.character.ord][self.character.abs + 1] == 'E'):
+                            nbr_objects = nbr_objects + 1
+                        self.display.mapf = self.character.right(self.display.mapf)
+                    if event.key == K_LEFT:
+                        if (self.display.mapf[self.character.ord][self.character.abs - 1] == 'N')\
+                        or (self.display.mapf[self.character.ord][self.character.abs - 1] == 'T')\
+                        or (self.display.mapf[self.character.ord][self.character.abs - 1] == 'E'):
+                            nbr_objects = nbr_objects + 1
+                        self.display.mapf = self.character.left(self.display.mapf)
+                    if event.key == K_UP:
+                        if (self.display.mapf[self.character.ord - 1][self.character.abs] == 'N')\
+                        or (self.display.mapf[self.character.ord - 1][self.character.abs] == 'T')\
+                        or (self.display.mapf[self.character.ord - 1][self.character.abs] == 'E'):
+                            nbr_objects = nbr_objects + 1
+                        self.display.mapf = self.character.up(self.display.mapf)
+                    if event.key == K_DOWN:
+                        if (self.display.mapf[self.character.ord + 1][self.character.abs] == 'N')\
+                        or (self.display.mapf[self.character.ord + 1][self.character.abs] == 'T')\
+                        or (self.display.mapf[self.character.ord + 1][self.character.abs] == 'E'):
+                            nbr_objects = nbr_objects + 1
+                        self.display.mapf = self.character.down(self.display.mapf)
+                    self.display.display_map()
 
         '''End of the game'''
         # end of the game
